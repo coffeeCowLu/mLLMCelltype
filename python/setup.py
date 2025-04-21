59d02f467c510c4a7512575fcf0046737f9b5f55
Chen Yang <cafferychen777@tamu.edu>
2025-04-21 16:12:33 -0500

    Fix duplicate python_requires in setup.py
    1. Removed duplicate python_requires parameter at the end of setup.py
    2. Kept only the python_requires='>=3.9' definition
    3. Removed Python 3.8 from classifiers list
    4. This fixes the SyntaxError: keyword argument repeated error

--- python/setup.py
+++ python/setup.py
@@ -60,10 +60,8 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
 )
